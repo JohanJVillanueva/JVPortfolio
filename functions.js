f5f60f71b24105f5fc667e87df6dea8f3ea57711
JohanJVillanueva <johanjvillanueva@iskolarngbayan.edu.ph>
2023-04-12 18:36:45 +0800

    separated files from html css and javascript

---
+++ functions.js
@@ -0,0 +1,27 @@
+
+var playSong = document.getElementById("pupsong"); 
+
+var aud = document.getElementById("pupsong");
+aud.onended = function() 
+{
+alert("The song has ended");
+window.location.reload()
+};
+
+function playAudio() { 
+    playSong.play(); 
+} 
+
+function pauseAudio() { 
+    playSong.pause(); 
+} 
+
+function disableBtn() {
+document.getElementById("playB").disabled = true;
+}
+
+function enableBtn() {
+document.getElementById("playB").disabled = false;
+}
+
+
